3c2447b70408287f2139d5f5555f44cec3806473
niiquash <qua16006@byui.edu>
2026-04-01 22:20:28 -0600

    move html files into jsx files along with styles.

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import HomePage from "./pages/home-page/HomePage";
+import CheckoutPage from "./pages/checkout-page/CheckoutPage";
+import OrdersPage from "./pages/orders-page/OrdersPage";
+import TrackingPage from "./pages/tracking-page/TrackingPage";
+import { Routes, Route } from "react-router";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+    </Routes>
+  );
 }
 
 export default App;
