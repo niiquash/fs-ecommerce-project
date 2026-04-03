5f14c62322dc908768ade7547b99912aa806c231
niiquash <qua16006@byui.edu>
2026-04-02 21:39:27 -0600

    Separate the checkout page header into its own file

--- ecommerce-frontend/src/pages/checkout-page/CheckoutPage.jsx
+++ ecommerce-frontend/src/pages/checkout-page/CheckoutPage.jsx
@@ -1,34 +1,12 @@
 import "./CheckoutPage.css";
-import "./CheckoutHeader.css";
-import { Link } from "react-router";
+import CheckoutHeader from "./CheckoutHeader";
 
 function CheckoutPage() {
   return (
     <>
+      <CheckoutHeader />
       <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
       <title>Checkout</title>
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </Link>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <Link className="return-to-home-link" to="/">
-              3 items
-            </Link>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
